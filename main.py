da8e9ea795615445b0894869c7265950c093f512
LYNXware <97060051+LYNXware@users.noreply.github.com>
2023-08-09 15:03:10 +0200

    screen_assignment

--- main.py
+++ main.py
@@ -33,6 +33,11 @@ from resource_path import resource_path
 from user import user
 
 
+import py_files.test
+import py_files.screen_help
+import py_files.screen_assignment
+# import py_files.screen_assignment
+
 
 
 class StartWindow(Screen):
@@ -708,72 +713,72 @@ class LayoutsWindowCustom(Widget):
         shutil.copytree(src, dest)
 
 
-class AssignmentWindow(Screen):
-    pass
+# class AssignmentWindow(Screen):
+#     pass
+#
+#
+# class AssignmentWindowCustom(Widget):
+#     # current_button gets values from DeviceButton
+#     current_button = DictProperty({'name': '', 'function': '', 'description': ''})
+#
+#     hexval_set = bytearray()
+#     function: str = ''
+#
+#     def assign_event(self, key, hexval):
+#         self.hexval_set += hexval
+#
+#         print(self.hexval_set)
+#
+#         if self.function != '':
+#             self.function = f'{self.function} + {key}'
+#         else:
+#             self.function = key
+#         self.ids.function.text = self.function
+#
+#     def reset(self):
+#         self.hexval_set = bytearray()
+#         self.function = ''
+#         self.ids.function.text = ''
+#         self.ids.description.text = ''
+#
+#     def save_button(self):
+#         name = self.current_button['name']
+#
+#         if not self.hexval_set:
+#             self.ids.function.text = '* * * no assignment * * *'
+#         else:
+#             pass
+#
+#         if name[0] == 'L':
+#             if user.setup.sublayer == 0:
+#                 user.current_layout.main_left[name].ascii_set = self.hexval_set
+#                 user.current_layout.main_left[name].function = self.function
+#                 user.current_layout.main_left[name].description = self.ids.description.text
+#             else:
+#                 user.current_layout.sub_left[name].ascii_set = self.hexval_set
+#                 user.current_layout.sub_left[name].function = self.function
+#                 user.current_layout.sub_left[name].description = self.ids.description.text
+#         else:
+#             if user.setup.sublayer == 0:
+#                 user.current_layout.main_right[name].ascii_set = self.hexval_set
+#                 user.current_layout.main_right[name].function = self.function
+#                 user.current_layout.main_right[name].description = self.ids.description.text
+#             else:
+#                 user.current_layout.sub_right[name].ascii_set = self.hexval_set
+#                 user.current_layout.sub_right[name].function = self.function
+#                 user.current_layout.sub_right[name].description = self.ids.description.text
+#
+#         # layout.save(setup.current_layout)
+#         user.current_layout.save(user.setup.active_layout)
 
 
-class AssignmentWindowCustom(Widget):
-    # current_button gets values from DeviceButton
-    current_button = DictProperty({'name': '', 'function': '', 'description': ''})
-
-    hexval_set = bytearray()
-    function: str = ''
-
-    def assign_event(self, key, hexval):
-        self.hexval_set += hexval
-
-        print(self.hexval_set)
-
-        if self.function != '':
-            self.function = f'{self.function} + {key}'
-        else:
-            self.function = key
-        self.ids.function.text = self.function
-
-    def reset(self):
-        self.hexval_set = bytearray()
-        self.function = ''
-        self.ids.function.text = ''
-        self.ids.description.text = ''
-
-    def save_button(self):
-        name = self.current_button['name']
-
-        if not self.hexval_set:
-            self.ids.function.text = '* * * no assignment * * *'
-        else:
-            pass
-
-        if name[0] == 'L':
-            if user.setup.sublayer == 0:
-                user.current_layout.main_left[name].ascii_set = self.hexval_set
-                user.current_layout.main_left[name].function = self.function
-                user.current_layout.main_left[name].description = self.ids.description.text
-            else:
-                user.current_layout.sub_left[name].ascii_set = self.hexval_set
-                user.current_layout.sub_left[name].function = self.function
-                user.current_layout.sub_left[name].description = self.ids.description.text
-        else:
-            if user.setup.sublayer == 0:
-                user.current_layout.main_right[name].ascii_set = self.hexval_set
-                user.current_layout.main_right[name].function = self.function
-                user.current_layout.main_right[name].description = self.ids.description.text
-            else:
-                user.current_layout.sub_right[name].ascii_set = self.hexval_set
-                user.current_layout.sub_right[name].function = self.function
-                user.current_layout.sub_right[name].description = self.ids.description.text
-
-        # layout.save(setup.current_layout)
-        user.current_layout.save(user.setup.active_layout)
-
-
-class HelpWindow(Screen):
-    version = f'LYNX app - Version {__version__}'
-
-    def open_link(self):
-        import webbrowser
-        webbrowser.open('https://www.lynx-workshop.com/')
-
+# class HelpWindow(Screen):
+#     version = f'LYNX app - Version {__version__}'
+#
+#     def open_link(self):
+#         import webbrowser
+#         webbrowser.open('https://www.lynx-workshop.com/')
+#
 
 class DeviceButton(DragBehavior, Button):
 
@@ -845,7 +850,7 @@ class MainApp(App):
         Builder.load_file(resource_path('kv_files/layouts.kv'))
         Builder.load_file(resource_path('kv_files/settings.kv'))
         Builder.load_file(resource_path('kv_files/theme.kv'))
-        Builder.load_file(resource_path('kv_files/help.kv'))
+        Builder.load_file(resource_path('kv_files/screen_help.kv'))
         Builder.load_file(resource_path('kv_files/custom-widgets .kv'))
 
         main_kv = Builder.load_file(resource_path('kv_files/main.kv'))
