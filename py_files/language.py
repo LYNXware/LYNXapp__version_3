b9b711af9765a49eee8291dd8ff9c1e766f1b33e
LYNXware <97060051+ApeDevil@users.noreply.github.com>
2023-09-11 23:56:32 +0200

    create lang

---
+++ py_files/language.py
@@ -0,0 +1,33 @@
+
+import json
+
+
+letters = {
+    'a': '\x61',
+    'b': '\x62',
+    'c': '\x63'
+}
+
+numbers = {
+    '0': 0x30,
+    '1': 0x31,
+    '2': 0x32
+}
+
+english_ascii = {
+    'letters': letters,
+    'numbers': numbers
+
+}
+
+print(english_ascii['letters']['a'])
+
+with open("english.json", "w") as outfile:
+    # write the dictionary to the file in JSON format
+    json.dump(english_ascii, outfile, indent=4)
+
+with open("english.json", "r") as infile:
+    # read the JSON data from the file into a dictionary
+    person = json.load(infile)
+
+print(person['letters']['a'])
